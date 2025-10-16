d424982f07359922c89fdf51f5699c158b779e38
6Esther22 <2917038286@qq.com>
2025-10-16 21:26:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# 这个文件用于编写代码
+s=input()
+letter=0
+digit=0
+space=0
+other=0
+for c in s:
+    if c.isalpha():
+        letter+=1
+    elif c.isdigit():
+        digit+=1
+    elif c.isspace():
+        space+=1
+    else:
+        other+=1
+print("英文字符：{}".format(letter))
+print("数字：{}".format(digit))
+print("空格：{}".format(space))
+print("其他字符：{}".format(other))
